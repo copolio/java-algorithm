97e8b1af59fa1a52bff72a1d4bc41ad83c991442
Luke <rlwjd1504@gmail.com>
2021-10-23 01:34:39 +0900

    문자열6:중복문자제거

---
+++ src/com/company/inflearn/string/Class06.java
@@ -0,0 +1,15 @@
+package com.company.inflearn.string;
+
+import java.util.Scanner;
+
+public class Class06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        String answer = "";
+        for (int i = 0; i < input.length(); i++) {
+            if (input.indexOf(input.charAt(i)) == i) answer += input.charAt(i);
+        }
+        System.out.print(answer);
+    }
+}
