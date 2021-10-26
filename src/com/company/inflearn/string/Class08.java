09a3bc787f6d3ef9472e4d90e2aff54cc78d52ed
Luke <rlwjd1504@gmail.com>
2021-10-26 23:20:52 +0900

    문자열8:유효한팰린드롬

---
+++ src/com/company/inflearn/string/Class08.java
@@ -0,0 +1,17 @@
+package com.company.inflearn.string;
+
+import java.util.Scanner;
+
+public class Class08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String s = input.toUpperCase().replaceAll("[^A-Z]", "");
+        String reversed = new StringBuilder(s).reverse().toString();
+        String answer = "NO";
+        if (s.equals(reversed)) {
+            answer = "YES";
+        }
+        System.out.print(answer);
+    }
+}
