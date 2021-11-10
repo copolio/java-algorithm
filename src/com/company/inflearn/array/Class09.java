8f70627d963a2bcabf016fc6ed12eefb3ce78a0f
Luke <rlwjd1504@gmail.com>
2021-11-10 23:11:15 +0900

    배열9:격자판최대합

---
+++ src/com/company/inflearn/array/Class09.java
@@ -0,0 +1,33 @@
+package com.company.inflearn.array;
+
+import java.util.Scanner;
+
+public class Class09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] table = new int[n][n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int cell = scanner.nextInt();
+                table[i][j] = cell;
+            }
+        }
+        int dLSum = 0, dRSum = 0;
+        for (int i = 0; i < n; i++) {
+            int hSum = 0, vSum = 0;
+            dLSum += table[i][i];
+            dRSum += table[i][n-i-1];
+            for (int j = 0; j < n; j++) {
+                hSum += table[i][j];
+                vSum += table[j][i];
+            }
+            max = Math.max(hSum, max);
+            max = Math.max(vSum, max);
+        }
+        max = Math.max(dLSum, max);
+        max = Math.max(dRSum, max);
+        System.out.print(max);
+    }
+}
