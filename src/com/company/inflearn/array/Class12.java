37775a91821b050ed513f49a0c549698f097caba
Luke <rlwjd1504@gmail.com>
2021-11-13 19:35:52 +0900

    배열12:진행중

---
+++ src/com/company/inflearn/array/Class12.java
@@ -0,0 +1,17 @@
+package com.company.inflearn.array;
+
+import java.util.Scanner;
+
+public class Class12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] table = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+    }
+}
