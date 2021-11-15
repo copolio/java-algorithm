6ec0c649b045752afd064a705bc03c40c3a7345f
Luke <rlwjd1504@gmail.com>
2021-11-15 20:10:52 +0900

    포인터01:두배열합치기

---
+++ src/com/company/inflearn/pointers/Class01.java
@@ -0,0 +1,36 @@
+package com.company.inflearn.pointers;
+
+import java.util.Scanner;
+
+public class Class01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] array1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            array1[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int[] array2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            array2[i] = scanner.nextInt();
+        }
+
+        int[] answer = new int[n + m];
+        int nCur = 0, mCur = 0;
+        for (int i = 0, j = n + m; i < j; i++) {
+            if (nCur < n && array1[nCur] <= array2[mCur]) {
+                answer[i] = array1[nCur];
+                nCur++;
+            } else {
+                answer[i] = array2[mCur];
+                mCur++;
+            }
+        }
+
+        for (int i = 0, j = n + m; i < j; i++) {
+            if (i > 0) System.out.print(" ");
+            System.out.print(answer[i]);
+        }
+    }
+}
