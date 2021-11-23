7b4f7f14b2ef5348f403bf7e52e208e6a66760c2
Luke <rlwjd1504@gmail.com>
2021-11-23 22:14:25 +0900

    해쉬03:매출액의종류

---
+++ src/com/company/inflearn/hash/Class03.java
@@ -0,0 +1,39 @@
+package com.company.inflearn.hash;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Class03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        ArrayList<Integer> sales = new ArrayList<>();
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            sales.add(scanner.nextInt());
+        }
+        for (int i = 0; i < k; i++) {
+            hash.put(sales.get(i), hash.getOrDefault(sales.get(i), 0) + 1);
+        }
+        answer.add(hash.size());
+        for (int i = 1; i <= n - k; i++) {
+            int count = hash.get(sales.get(i - 1)) - 1;
+            if (count == 0) {
+                hash.remove(sales.get(i - 1));
+            } else {
+                hash.remove(sales.get(i - 1));
+                hash.put(sales.get(i - 1), count);
+            }
+            hash.put(sales.get(i + k - 1), hash.getOrDefault(sales.get(i + k - 1), 0) + 1);
+            answer.add(hash.size());
+        }
+
+        for (int i = 0; i < answer.size(); i++) {
+            if (i > 0) System.out.print(" ");
+            System.out.print(answer.get(i));
+        }
+    }
+}
