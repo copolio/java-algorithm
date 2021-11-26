fc740d4b640aac040f0d44d96944880c59bb5c7c
Luke <rlwjd1504@gmail.com>
2021-11-26 21:09:59 +0900

    스택01:올바른 괄호

---
+++ src/com/company/inflearn/stack/Class01.java
@@ -0,0 +1,28 @@
+package com.company.inflearn.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Class01 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        Stack<Character> stack = new Stack<>();
+        String answer = "NO";
+        while(!input.isEmpty()) {
+            if (input.charAt(0) == ')') {
+                if (stack.empty()) {
+                    break;
+                }
+                stack.pop();
+            } else {
+                stack.add(input.charAt(0));
+            }
+            input = input.substring(1);
+        }
+        if (stack.empty() && input.isEmpty()) {
+            answer = "YES";
+        }
+        System.out.print(answer);
+    }
+}
