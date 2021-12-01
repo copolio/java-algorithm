88981aacd970247b42ca068732bb4e5958dc6701
Luke <rlwjd1504@gmail.com>
2021-12-01 21:57:36 +0900

    Fix: Change Package Name

--- src/com/company/inflearn/stackqueue/Class01.java
+++ src/com/company/inflearn/stackqueue/Class01.java
@@ -1,4 +1,4 @@
-package com.company.inflearn.stack;
+package com.company.inflearn.stackqueue;
 
 import java.util.Scanner;
 import java.util.Stack;
--- src/com/company/inflearn/stackqueue/Class06.java
+++ src/com/company/inflearn/stackqueue/Class06.java
@@ -1,4 +1,4 @@
-package com.company.inflearn.stack;
+package com.company.inflearn.stackqueue;
 
 import java.util.LinkedList;
 import java.util.Scanner;
