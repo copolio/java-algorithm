0024e29af45ccae4ed9fae9abf672b181787ae0d
Luke <rlwjd1504@gmail.com>
2021-12-03 20:24:42 +0900

    스택08:응급실

---
+++ src/com/company/inflearn/stackqueue/Class08.java
@@ -0,0 +1,43 @@
+package com.company.inflearn.stackqueue;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Class08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int answer = 0;
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            queue.offer(scanner.nextInt());
+        }
+
+        int curPos = m;
+        while (true) {
+            Integer first = queue.getFirst();
+            queue.remove();
+            Integer max = Collections.max(queue);
+            if (first < max) {
+                if (curPos == 0) {
+                    curPos = queue.size();
+                } else {
+                    curPos--;
+                }
+                queue.offer(first);
+            } else {
+                answer++;
+                if (curPos == 0) {
+                    break;
+                } else {
+                    curPos--;
+                }
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
