16da44b73380089b4c3a4b75745642f68634f2e1
Luke <rlwjd1504@gmail.com>
2021-12-04 17:11:16 +0900

    정렬/탐색01: 선택정렬

---
+++ src/com/company/inflearn/sortsearch/Class01.java
@@ -0,0 +1,24 @@
+package com.company.inflearn.sortsearch;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Class01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Integer> array = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            array.add(scanner.nextInt());
+        }
+
+        Collections.sort(array);
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0) System.out.print(' ');
+            System.out.print(array.get(i));
+        }
+    }
+}
