eb0c41c4d8850d418f2b8d7ccaaef978ae1609b5
Luke <rlwjd1504@gmail.com>
2021-12-12 14:55:18 +0900

    정렬/탐색11. 송아지 찾기 1(BFS : 상태트리탐색)

---
+++ src/com/company/inflearn/sortsearch/Class11.java
@@ -0,0 +1,36 @@
+package com.company.inflearn.sortsearch;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Class11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
+        int e = scanner.nextInt();
+        int[] moves = {5, 1, -1};
+        int[] visited = new int[10001];
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(s);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (cur == (e)) {
+                break;
+            }
+            for (int i = 0; i < 3; i++) {
+                int next = cur + moves[i];
+                if (next < 1 || next > 10000 || next == s) {
+                    continue;
+                }
+                if (visited[next] == 0) {
+                    q.offer(next);
+                    visited[next] = visited[cur] + 1;
+                }
+            }
+        }
+        System.out.print(visited[e]);
+    }
+}
