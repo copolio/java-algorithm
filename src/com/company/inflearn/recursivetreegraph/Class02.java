6cb7b380ee47a765174efc3d8f10ee174cd4b415
Luke <rlwjd1504@gmail.com>
2021-12-14 20:37:48 +0900

    Recursive, Tree, Graph(DFS, BFS 기초)2. 바둑이 승차(DFS)

---
+++ src/com/company/inflearn/recursivetreegraph/Class02.java
@@ -0,0 +1,42 @@
+package com.company.inflearn.recursivetreegraph;
+
+import java.util.Scanner;
+
+public class Class02 {
+    int maxSize = 0;
+    int limit = 0;
+
+    public void dfs(int[] graph, boolean[] visited, int cur, int sum) {
+        if (sum > limit) {
+            return;
+        }
+        if (sum > maxSize) {
+            maxSize = sum;
+        }
+        for (int i = cur; i < graph.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(graph, visited, i, sum+graph[i]);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Class02 class02 = new Class02();
+
+        Scanner scanner = new Scanner(System.in);
+        class02.limit = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[] graph = new int[n];
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = scanner.nextInt();
+        }
+        class02.dfs(graph, visited, 0, 0);
+        System.out.println(class02.maxSize);
+    }
+}
+
+
